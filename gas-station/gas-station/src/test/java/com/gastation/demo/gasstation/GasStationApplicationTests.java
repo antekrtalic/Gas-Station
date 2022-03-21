4f662ee9278d0f8f21c1d2c3e5778f2a2262e37c
Ante Dragoje <antekrtalic2@gmail.com>
2022-03-21 21:52:52 +0100

    Working on my project, and setting dafault spring boot dependencies

---
+++ gas-station/gas-station/src/test/java/com/gastation/demo/gasstation/GasStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastation.demo.gasstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
