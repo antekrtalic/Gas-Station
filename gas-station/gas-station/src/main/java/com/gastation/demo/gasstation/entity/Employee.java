186964c5baf60a256a339caf73330addd4cb4b1b
Ante Dragoje <antekrtalic2@gmail.com>
2022-03-27 21:56:25 +0200

    Adding constructors for Employee entity

--- gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/Employee.java
+++ gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/Employee.java
@@ -13,7 +13,18 @@ public class Employee {
     @Column(name = "firstName")
     private String firstName;
 
+    @Column(name = "lastName")
     private String lastName;
 
+    @Column(name = "role")
     private String role;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
 }
