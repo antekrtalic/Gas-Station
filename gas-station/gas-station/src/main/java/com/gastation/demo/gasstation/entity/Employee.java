b57404fc4f2649ee5ebc29bdc0f3b0e02bd542dc
Ante Dragoje <antekrtalic2@gmail.com>
2022-03-27 21:55:05 +0200

    Adding columns to specific fields for employees

--- gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/Employee.java
+++ gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/Employee.java
@@ -1,9 +1,19 @@
 package com.gastation.demo.gasstation.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    private String lastName;
+
+    private String role;
 }
