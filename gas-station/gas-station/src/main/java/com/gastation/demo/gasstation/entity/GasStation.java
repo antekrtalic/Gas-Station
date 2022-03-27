c1ac926a3e7a73be6c76941675fa31e40ccf1e63
Ante Dragoje <antekrtalic2@gmail.com>
2022-03-27 21:48:09 +0200

    Adding constructors, getters and setters for GasStation Entity

--- gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/GasStation.java
+++ gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/GasStation.java
@@ -26,4 +26,80 @@ public class GasStation {
 
     private String disel;
 
+    public GasStation() {
+    }
+
+    public GasStation(List<Employee> employees, String city, String address, String country, String nintyFiveBMB, String nintyEightBMB, String disel) {
+        this.employees = employees;
+        this.city = city;
+        this.address = address;
+        this.country = country;
+        this.nintyFiveBMB = nintyFiveBMB;
+        this.nintyEightBMB = nintyEightBMB;
+        this.disel = disel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getNintyFiveBMB() {
+        return nintyFiveBMB;
+    }
+
+    public void setNintyFiveBMB(String nintyFiveBMB) {
+        this.nintyFiveBMB = nintyFiveBMB;
+    }
+
+    public String getNintyEightBMB() {
+        return nintyEightBMB;
+    }
+
+    public void setNintyEightBMB(String nintyEightBMB) {
+        this.nintyEightBMB = nintyEightBMB;
+    }
+
+    public String getDisel() {
+        return disel;
+    }
+
+    public void setDisel(String disel) {
+        this.disel = disel;
+    }
 }
