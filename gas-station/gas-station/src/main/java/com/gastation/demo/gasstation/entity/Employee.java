afec373f2a2f6523829381bf5a0858b9b44b2e75
Ante Dragoje <antekrtalic2@gmail.com>
2022-03-28 22:56:42 +0200

    Added getters and setters for entity class Employee

--- gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/Employee.java
+++ gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/entity/Employee.java
@@ -27,4 +27,36 @@ public class Employee {
         this.lastName = lastName;
         this.role = role;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
