ed12323f1a1c426b680eb1c84b0927aaac836ab4
Ante Dragoje <antekrtalic2@gmail.com>
2022-04-02 20:40:14 +0200

    Add fields for dto gas station

--- gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/dto/GasStation.java
+++ gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/dto/GasStation.java
@@ -1,4 +1,11 @@
 package com.gastation.demo.gasstation.dto;
 
+import com.gastation.demo.gasstation.entity.Employee;
+
+import java.util.List;
+
 public class GasStation {
+
+    private Long id;
+    private List<Employee> employees;
 }
