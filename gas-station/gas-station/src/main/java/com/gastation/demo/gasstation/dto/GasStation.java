84999a84dada302c7a38b33e619f06abe3fa20ce
Ante Dragoje <antekrtalic2@gmail.com>
2022-04-14 20:15:48 +0200

    Add get method on gas station dto id

--- gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/dto/GasStation.java
+++ gas-station/gas-station/src/main/java/com/gastation/demo/gasstation/dto/GasStation.java
@@ -1,12 +1,15 @@
 package com.gastation.demo.gasstation.dto;
 
 import com.gastation.demo.gasstation.entity.Employee;
+import lombok.Getter;
 
 import java.util.List;
 
 public class GasStation {
 
+    @Getter
     private Long id;
+
     private List<Employee> employees;
 
     private String city;
